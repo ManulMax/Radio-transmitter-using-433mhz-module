3174b0a720928281e7ceb8f8e9b7dcff85e748f8
Manul Madara <49731724+ManulMax@users.noreply.github.com>
2019-09-24 01:31:35 +0530

    Add files via upload

---
+++ Radio_sender/Radio_sender.ino
@@ -0,0 +1,39 @@
+#include <RCSwitch.h>
+
+#define joyx A0
+#define joyy A1
+
+RCSwitch myswi=RCSwitch();
+void setup() {
+ myswi.enableTransmit(10);
+ Serial.begin(9600);
+}
+boolean b=true;
+
+void loop() {
+  int x=analogRead(joyx);
+  delay(10);
+  Serial.println(x);
+  int y=analogRead(joyy);
+  delay(10);
+  Serial.println(y);
+  
+if (b==true) {
+  if(x>1000 && y<510){
+    myswi.send("001"); //1 to forwrd
+    }
+  else if(x<10 && y<510){
+    myswi.send("010"); //2 for backword
+    }
+  else if(y>1000 && x>500){
+    myswi.send("011");  //3 for right
+  }  
+  else if(x>500 && y<10){
+    myswi.send("100"); // 4 for left
+    }
+  }
+
+else{
+  myswi.send("000");
+    }
+}
---
+++ radio_reciever/radio_reciever.ino
@@ -0,0 +1,22 @@
+#include <RCSwitch.h>
+RCSwitch myswi=RCSwitch();
+
+void setup() {
+Serial.begin(9600);
+myswi.enableReceive(8);  
+}
+ 
+void loop() {
+if (myswi.available()){
+  int val = myswi.getReceivedValue();
+  Serial.println(myswi.getReceivedValue());
+  if (val==0){
+    Serial.print("Unknown encoding");
+    }
+  else{
+    Serial.println(myswi.getReceivedValue());
+    }  
+  myswi.resetAvailable();
+  }  
+
+}
